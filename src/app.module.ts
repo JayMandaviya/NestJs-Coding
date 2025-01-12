266321e38a23c9263f1f47a0d1bd738bbb4579ae
Jay Mandaviya <61206322+JayMandaviya@users.noreply.github.com>
2025-01-12 11:12:10 +0530

    Modules added and some basic code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { IngestionModule } from './ingestion/ingestion.module';
+import { DocumentModule } from './document/document.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(),
+    AuthModule,
+    UserModule,
+    DocumentModule,
+    IngestionModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
